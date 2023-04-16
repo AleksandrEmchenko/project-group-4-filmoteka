0156113efc01be736996c45df6a6c2e1806ff5ee
juliabykovaa <97822247+juliabykovaa@users.noreply.github.com>
2023-04-16 20:12:47 +0300

    film modal fixed

--- src/js/renderFilmModal.js
+++ src/js/renderFilmModal.js
@@ -2,48 +2,52 @@ import { setToLocalStorage } from './localStorage';
 
 const API_KEY = '351363cde76d5d5ccd40418b50fed610';
 const modal = document.querySelector('[data-modal]');
-const modalContent = document.querySelector('.film_modal-content');
 const testButton = document.querySelector('.test_button');
- const modalWrap = document.querySelector('.modal__wrap');
+const modalWrap = document.querySelector('.modal__wrap');
 
 testButton.addEventListener('click', makeModal);
 
 function makeModal(event) {
-    event.preventDefault()
-    fetchOneMovieInfo(550).then((data) => {
-        openModal(data);
-        modal.addEventListener('click', onBackDropClick);
-        document.addEventListener('keydown', onEscKeyPress);
-        const closeBtn = modal.querySelector('.film_modal__close-btn');
-        closeBtn.addEventListener('click', onCloseModal);
-    });
+  event.preventDefault()
+  // event.currentTarget.dataset.id;
+  const movieId = 577; 
+
+  fetchOneMovieInfo(movieId).then((data) => {
+    
+    openModal(data);
+    modal.addEventListener('click', onBackDropClick);
+    document.addEventListener('keydown', onEscKeyPress);
+    const closeBtn = modal.querySelector('.film_modal__close-btn');
+    closeBtn.addEventListener('click', onCloseModal);
+   updateQeueuButtonText(movieId); updateQeueuButtonText
+    updateWatchedButtonText(movieId)
+  });
 }
 
-function fetchOneMovieInfo(movie_id) {
+async function fetchOneMovieInfo(movie_id) {
   const url = `https://api.themoviedb.org/3/movie/${movie_id}?api_key=${API_KEY}`;
-  return fetch(url)
-    .then((response) => response.json())
-    .then((data) => ({
-      ...data,
-        popularity: data.popularity.toFixed(2),
-
-    }));
+  const response = await fetch(url);
+  const data = await response.json();
+  return {
+    ...data,
+    popularity: data.popularity.toFixed(2)
+  };
 }
 
 function renderMovieCard(data) {
-
-    const { title, vote_count, popularity, original_title, overview, poster_path, genres, vote_average } =
-        data;
-    const genresFormatted = genres.map((genre) => genre.name).join(', ');
-    const movieCardMarkup = `
+  const { title, vote_count, popularity, original_title, overview, poster_path, genres, vote_average } =
+    data;
+  const genresFormatted = genres.map((genre) => genre.name).join(', ');
+  const movieCardMarkup = `
+  
     <img class="film_modal-img" src="${`https://www.themoviedb.org/t/p/w500${poster_path}`}" alt="${title}">
     <div class="film_modal-about">
       <h1 class="film_modal-title">${title}</h1>
       <div class="film_modal-info">
         <div class="film_modal__info-header">
-          <p class="info-header">Vote<span> / </span>Votes</p>
+          <p class="info-header">Vote<span>/</span>Votes</p>
           <p class="info-header">Popularity</p>
-          <p class="info-header">Original Title</p>
+          <p class="info-header ">Original Title</p>
           <p class="info-header">Genre</p>
         </div>
         <div class="film_modal__info-value">
@@ -53,29 +57,42 @@ function renderMovieCard(data) {
             <span class="info-value__text">${vote_count}</span>
           </p>
           <p class="info-value">${popularity}</p>
-          <p class="info-value">${original_title}</p>
+          <p class="info-value original-title">${original_title}</p>
           <p class="info-value">${genresFormatted}</p>
         </div>
       </div>
       <h2 class="film_modal-desc">ABOUT</h2>
       <p class="film_modal__desc-text">${overview}</p>
       <ul class="film_btn-list">
-        <li class="film__btn-item">
-          <button class="film_btn add-to-watched" type="button">Add to Watched</button>
-        </li>
-        <li class="film__btn-item">
-          <button class="film_btn add-to-queue" type="button">Add to queue</button>
-        </li>
-      </ul>
+            <li class="film__btn-item">
+                <button class="film_btn added add-to-watched" type="button">Add to Watched</button>
+            </li>
+            <li class="film__btn-item">
+                <button class="film_btn add-to-queue" type="button">Add to queue</button>
+            </li>
+        </ul>
+      
     </div>`;
 
-    modalWrap.insertAdjacentHTML('beforeend', movieCardMarkup);
+  modalWrap.insertAdjacentHTML('beforeend', movieCardMarkup);
+
+  const avarangeVote = document.querySelector('.avarange_vote');
+  if (vote_average >= 5) {
+    avarangeVote.classList.add('good-rated')
+  } else {
+    avarangeVote.classList.add('bad-rated')
+  }
 
   const addToWatchedBtn = document.querySelector('.add-to-watched');
   const addToQueueBtn = document.querySelector('.add-to-queue');
-
   addToWatchedBtn.addEventListener('click', (event) => setToLocalStorage(event, data));
   addToQueueBtn.addEventListener('click', (event) => setToLocalStorage(event, data));
+  addToQueueBtn.addEventListener('click', function() {
+      updateQeueuButtonText(data.id);
+    });
+  addToWatchedBtn.addEventListener('click', function () {
+    updateWatchedButtonText(data.id)
+})
 }
 
 function openModal(data) {
@@ -90,18 +107,55 @@ function onCloseModal() {
   document.removeEventListener('keydown', onEscKeyPress);
 }
 
-  function onBackDropClick(event) {
+function onBackDropClick(event) {
   if (event.currentTarget === event.target) {
-    onCloseModal();
-  }
+  modal.classList.add('is-hidden');
+  modalWrap.innerHTML = '';
   modal.removeEventListener('click', onBackDropClick);
   document.removeEventListener('keydown', onEscKeyPress);
+  }
+  return;
 }
 
-   function onEscKeyPress(event) {
-    if (event.code !== 'Escape') {
-      return;
-    }
-    window.removeEventListener('keydown', onEscKeyPress);
-    onCloseModal();
+function onEscKeyPress(event) {
+  if (event.code !== 'Escape') {
+    return;
+  }
+  modalWrap.innerHTML = '';
+  modal.classList.add('is-hidden');
+  modal.removeEventListener('click', onBackDropClick);
+  document.removeEventListener('keydown', onEscKeyPress);  
+}
+
+
+function updateQeueuButtonText(id) {
+  
+const addToQueueBtn = document.querySelector('.add-to-queue');
+
+  const localstorage = localStorage.getItem('QUEUE');
+
+  if (localstorage === null) {
+    addToQueueBtn.textContent = 'Add to queue';
+    return;
+  }
+  if (JSON.parse(localstorage.includes(id))) {
+    addToQueueBtn.textContent = 'Remove from queue';
+  } else {
+    addToQueueBtn.textContent = 'Add to queue';
+  }
+}
+
+function updateWatchedButtonText(id) {
+  const addToWatchedBtn = document.querySelector('.add-to-watched');
+  const localstorageWatched = localStorage.getItem('WATCHED');
+
+  if (localstorageWatched === null) {
+    addToWatchedBtn.textContent = 'Add to watched';
+    return;
+  }
+  if (JSON.parse(localstorageWatched.includes(id))) {
+    addToWatchedBtn.textContent = 'Remove from watched';
+  } else {
+    addToWatchedBtn.textContent = 'Add to watched';
+  }
 }
