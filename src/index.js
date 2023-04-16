0c188712e1e7fd332a9435b64c82c183f79f1022
AleksandrEmchenko <aleksandremchenko@gmail.com>
2023-04-16 16:21:07 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ import "./js/requests.js";
 import "./js/init.js";
 import "./js/buttonUp.js";
 
-import "./js/firebaseInit.js";
-import "./js/firebaseAuth.js";
-import "./js/authModal.js";
+// import "./js/firebaseInit.js";
+// import "./js/firebaseAuth.js";
+// import "./js/authModal.js";
