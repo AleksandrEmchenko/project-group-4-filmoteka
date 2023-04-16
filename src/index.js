fe9f1c8184d874647653fa5555aba3fa81041276
AleksandrEmchenko <aleksandremchenko@gmail.com>
2023-04-16 16:23:46 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import "./js/membersModal.js";
 import "./js/createCards.js";
 import "./js/loaderSpinner.js";
 import "./js/requests.js";
-import "./js/init.js";
+// import "./js/init.js";
 import "./js/buttonUp.js";
 
 // import "./js/firebaseInit.js";
