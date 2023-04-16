bcdee8c6369564f4d0e5d86e0c7d179161502ada
AleksandrEmchenko <aleksandremchenko@gmail.com>
2023-04-16 16:28:01 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 // import "./js/renderFilmModal.js";
 // import "./js/membersModal.js";
-import "./js/createCards.js";
-import "./js/loaderSpinner.js";
-import "./js/requests.js";
+// import "./js/createCards.js";
+// import "./js/loaderSpinner.js";
+// import "./js/requests.js";
 // import "./js/init.js";
-import "./js/buttonUp.js";
+// import "./js/buttonUp.js";
 
 // import "./js/firebaseInit.js";
 // import "./js/firebaseAuth.js";
