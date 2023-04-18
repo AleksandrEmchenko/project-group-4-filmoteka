b31455c23fe477049e8d4a18dc8764d0a99f098c
Aleksandr Yemchenko <aleksandremchenko@gmail.com>
2023-04-19 00:38:01 +0300

    Update btn-theme.js

--- src/js/btn-theme.js
+++ src/js/btn-theme.js
@@ -1,34 +1,28 @@
 const btnThemeHeader = document.querySelector("#switch1");
 const bodyTheme = document.querySelector("body");
-const checked = document.querySelector(".theme-checkbox");
+const checked = document.querySelector(".switch");
 let theme = localStorage.getItem("ui-theme");
 
+
 window.addEventListener("load", saveTheme);
-
-if (btnThemeHeader !== null) {
-    btnThemeHeader.addEventListener("click", onTheme);
-}
-
+btnThemeHeader.addEventListener('click', onTheme);
 
 function saveTheme() {
  if (theme === "dark") {
   bodyTheme.classList.add("body-theme");
-  if (checked !== null) {
-    checked.setAttribute("checked", true);
-  }
-  
- }
+  checked.setAttribute("checked", true);
+	};
 }
 
 function onTheme() {
  theme = localStorage.getItem("ui-theme");
 
- if (theme === "dark") {
-  bodyTheme.classList.remove("body-theme");
-  localStorage.setItem("ui-theme", "light");
-  return;
- }
+	if (theme === "dark") {
+		bodyTheme.classList.remove("body-theme");
+		localStorage.setItem("ui-theme", "light");
+		return;
+	} 
 
- bodyTheme.classList.add("body-theme");
- localStorage.setItem("ui-theme", "dark");
+ 	bodyTheme.classList.add("body-theme");
+	localStorage.setItem("ui-theme", "dark");	
 }
